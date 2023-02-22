025497287e739d3abad6170ce0709c085a590255
Juan Lopez <54092513+jlopz135@users.noreply.github.com>
2023-02-21 21:19:46 -0500

    Adding starter code, linked to discord

---
+++ FAQBot.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+import json
+import os
+
+if os.path.exists(os.getcwd() + "/config.json"):
+    with open("./config.json") as f:
+        configData = json.load(f)
+
+else:
+    configTemplate = {"Token": "", "Prefix": "!"}
+
+    with open(os.getcwd() + "/config.json", "w+") as f:
+        json.dump(configTemplate, f) 
+
+token = configData["Token"]
+prefix = configData["Prefix"]
+
+ 
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+print("here")
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+ 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+ 
+    if message.content.startswith('!'):
+        await message.channel.send('Message Read!')
+ 
+client.run(token)
