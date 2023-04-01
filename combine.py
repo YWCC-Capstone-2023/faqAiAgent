30d89a3a282a6434401bd1b77cab0f3c0e80ae9f
Juan Lopez <juanclopez135@gmail.com>
2023-04-01 00:22:58 -0400

    cleaned up combine.py

--- combine.py
+++ combine.py
@@ -3,7 +3,6 @@ import pygsheets
 import discord
 from discord.ext import commands
 from oauth2client.service_account import ServiceAccountCredentials
-import asyncio
 from thefuzz import process, fuzz
 from difflib import SequenceMatcher
 
@@ -14,8 +13,6 @@ creds = ServiceAccountCredentials.from_json_keyfile_name(
     'service_account_credentials.json', scope)
 
 df = pd.read_csv("https://docs.google.com/spreadsheets/d/1m51HUH0AQi28EBnsLwP9gasUHPuLVzFuNu1L4N6Zs-Y/gviz/tq?tqx=out:csv&sheet=Question+and+Answers_new")
-
-df1 = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTEeagPjMdSxmySlZ-_DmFktpi_ynHE8USFhDygazEScxnQWCIupFZ1z1ygfc5w29_g7sGa0FIFy1TK/pub?gid=0&single=true&output=csv")
 
 gc = pygsheets.authorize(service_file='service_account_credentials.json')
 
@@ -38,11 +35,9 @@ intents.message_content = True
 client = discord.Client(command_prefix='!', intents=intents)
 bot = commands.Bot(command_prefix='/', intents=intents, case_insensitive = True)
 
-questions = df["Questions"]
 
 
 def addMe(q,a):
-    # if changes update df
     sh = gc.open('Question and Answers_new')
     print(f"Opened sheet {sh}")
     worksheet1 = sh[0]
@@ -59,15 +54,14 @@ def measure_accuracy(fuzz_ratio:int, seq_match:float) -> bool:
     acc_thresh = 70
 
     distance = abs(fuzz_ratio - round(seq_match))
-    
+    # print(f"Distance: {distance}")
     if fuzz_ratio < acc_thresh or seq_match < acc_thresh / 100:
         return False
+
     if distance > distance_thresh:
         return False
 
     return True
-
-questions = trim_all_columns(questions)
 
 @bot.event
 async def on_ready():
@@ -81,18 +75,23 @@ async def list(ctx):
 
 @bot.command()
 async def ask(ctx, *, content:str):
+    df = pd.read_csv("https://docs.google.com/spreadsheets/d/1m51HUH0AQi28EBnsLwP9gasUHPuLVzFuNu1L4N6Zs-Y/gviz/tq?tqx=out:csv&sheet=Question+and+Answers_new")
+    questions = df["Questions"]
+    questions = trim_all_columns(questions)
+
     if ctx.author == bot.user:
         return
     
     message = content
 
     # print(message)
-    ans = process.extractOne(message, questions ,scorer=fuzz.token_set_ratio) #get the answer for the question that it most closley resembles
+    '''get the answer for the question that it most closley resembles'''
+    ans = process.extractOne(message, questions ,scorer=fuzz.token_set_ratio) 
     # print(f"{ans}\n")
 
     response = df.iloc[ans[2]][1]
 
-    # print(f"Question: {ans[0]}\n")
+    print(f"Question: {ans[0]}\n")
 
     print(f"Sequence Match: {SequenceMatcher(None, message, ans[0]).ratio()}")
     print(f"Fuzz accuracy: {ans[1]}")
@@ -110,16 +109,16 @@ async def ask(ctx, *, content:str):
 async def add(ctx, *, content:str):
     if ctx.author == bot.user:
         return
-   
+    
+    '''Cleaning the message to input into DB'''
     a = content.find("?")
-
     user_question = content[:a+1]
     user_answer = content[a+1:].strip()
     
     print(f"Question: {user_question}\n")
     print(f"Answer: {user_answer}\n")
     addMe(user_question,user_answer)
-  
+
     await ctx.reply(f'Hi {ctx.message.author.mention}! We have added this question and answer in our Database.')
 
 bot.run(token)
