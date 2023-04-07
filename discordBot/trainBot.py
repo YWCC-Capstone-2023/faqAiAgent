09cb6a1eabaa052caa2a267e76b04f73cf50f36b
Juan Lopez <juanclopez135@gmail.com>
2023-04-07 15:49:35 -0400

    Added to gitignore

--- discordBot/trainBot.py
+++ discordBot/trainBot.py
@@ -5,9 +5,5 @@ from functions import get_intents
 
 SPREADSHEET = "https://docs.google.com/spreadsheets/d/1m51HUH0AQi28EBnsLwP9gasUHPuLVzFuNu1L4N6Zs-Y/gviz/tq?tqx=out:csv&sheet=Question+and+Answers_new"
 
-if os.path.exists(os.getcwd() + "/intents.json"):
-    intents_path = os.getcwd() + "/intents.json"
-else:
-    #create intents file
-    get_intents(SPREADSHEET)
+get_intents(SPREADSHEET)
 
