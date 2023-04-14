fe7a7c1a70449e37f37cefd4b318862e91ace75d
vap49 <82918026+vap49@users.noreply.github.com>
2023-04-14 02:36:28 -0400

    created modal for add command

--- cogs/add.py
+++ cogs/add.py
@@ -3,15 +3,43 @@ import discord
 import pygsheets
 import os
 
+class AddModal(discord.ui.Modal, title = "Add a Question and Answer"):
+    question = discord.ui.TextInput(
+        label = 'Question', 
+        required=True, 
+        placeholder="Input Question Here",
+        style=discord.TextStyle.paragraph
+    )
+
+    answer = discord.ui.TextInput(
+        label = 'Answer', 
+        required=True, 
+        placeholder="Input Answer Here",
+        style=discord.TextStyle.paragraph
+    )
+    
+    tag = discord.ui.TextInput(
+        label = 'Tag',
+        placeholder="Input TagHere",
+        style=discord.TextStyle.short,
+        default="default_tag"
+    )
+    
+    async def on_submit(self, interaction: discord.Interaction, /) -> None:
+        q = self.question
+        a = self.answer
+        t = self.tag
+
+        addMe(q=q, a=a, t=t)
+        await interaction.response.send_message(f"{interaction.user.mention} Thank You! This set has been added to the sheet!")
+    
+    async def on_error(self, interaction: discord.Interaction, error: Exception, /) -> None:
+        return await super().on_error(interaction, error)
+
+
 class Add(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-        if os.path.exists('credentials/service_account_credentials.json'):
-            self.gc = pygsheets.authorize(service_file='credentials/service_account_credentials.json')
-        else:
-            pass
-
         print("Add Loaded\n")
 
     @commands.hybrid_command(name="add", description= "Add a question, answer, topic set to the database!",guild_ids=[1072948383955816459])
@@ -21,8 +49,7 @@ class Add(commands.Cog):
         print(f"Answer: {answer}\n")
         print(f'Topic: {topic}')
 
-        self.addMe(question,answer,topic)
-        await ctx.reply(f"{ctx.author.mention} We have added this question and answer in our Database.", ephermal = True)
+        await ctx.response.send_modal(AddModal())
 
     @add.error
     async def __add_error(self, ctx:commands.Context, error):
@@ -31,12 +58,15 @@ class Add(commands.Cog):
         else:
             await ctx.reply(f"Sorry {ctx.author.mention},I do not understand! Please ping the Professor!", ephemeral=True)
 
-    def __addMe(self,q,a,t='default_tag'):
-        sh = self.gc.open('Question and Answers_new')
-        worksheet1 = sh[0]
-        worksheet1.append_table([q,a,t], start='A104') #list should be question and answer
+def addMe(self,q,a,t='default_tag'):
+    if os.path.exists('../credentials/service_account_credentials.json'):
+        self.gc = pygsheets.authorize(service_file='../credentials/service_account_credentials.json')
+    else: pass
+
+    sh = self.gc.open('Question and Answers_new')
+    worksheet1 = sh[0]
+    worksheet1.append_table([q,a,t], start='A104') #list should be question and answer
 
 
-    
 async def setup(bot):
     await bot.add_cog(Add(bot))
--- cogs/ask.py
+++ cogs/ask.py
@@ -1,16 +1,29 @@
+import os, json, pandas as pd
+
 from discord.ext import commands
 import discord
 
-from trainAgent import agent
+from neuralintents import GenericAssistant
 
 class Ask(commands.Cog):
+    SPREADSHEET = "https://docs.google.com/spreadsheets/d/1m51HUH0AQi28EBnsLwP9gasUHPuLVzFuNu1L4N6Zs-Y/gviz/tq?tqx=out:csv&sheet=Question+and+Answers_new"
+    PATH_TO_INTENTS = '../intents/intents.json'
+    
     def __init__(self, bot) -> None:
         self.bot = bot
         print("Ask Loaded\n")
 
+        self.__train_agent()
+
     @commands.hybrid_command(name='ask', description="Ask the bot a question!", guild_ids=[1072948383955816459])
     async def ask(self, interaction: discord.Interaction, question:str):
-        response = agent.request(question)
+        """Ask the Bot a question
+
+        Args:
+            interaction (discord.Interaction): Context Manager
+            question (str): question user passes to command via discord client
+        """
+        response = self.agent.request(question)
         await interaction.reply(f"Hi, {interaction.author.mention}! {response}", ephemeral=True)
 
     @ask.error
@@ -19,6 +32,83 @@ class Ask(commands.Cog):
             await ctx.reply(f"You do not have permission to do that!", ephemeral=True)
         else:
             await ctx.reply(f"Sorry {ctx.author.mention},I do not understand! Please ping the Professor!", ephemeral=True)
+    
+    def __convert_col_to_list(self, df:pd.DataFrame, col_name:str, new_col_name:str = "") -> pd.DataFrame:
+        """
+        Converts every row of a column in a pandas DataFrame into a list containing
+        strings delimited by a semi-colon.
+        
+        Args:
+            df (pandas.DataFrame): The DataFrame containing the column to be converted.
+            col_name (str): The name of the column to be converted.
+            new_col_name (str): The new name of the column to be converted. Defaults to original name
+        
+        Returns:
+            pandas.DataFrame: A new DataFrame with the converted column.
+        """
+        if new_col_name == "": new_col_name = col_name
+        
+        df[new_col_name] = df[col_name].apply(lambda x: str(x).strip().split(";"))
+        
+        return df
+    
+    def __load_spreadsheet_as_intents(self,df:pd.DataFrame, new_filename:str = "../intents/intents.json") -> None:
+        """
+        Writes to a file a DataFrame as a json object
+
+        Args:
+            df (pandas.DataFrame) : The dataframe to be written to the file
+            new_filename (str) : The name of the file, Default = intents.json
+        """
+        df = self.__convert_col_to_list(df, "patterns")
+        df = self.__convert_col_to_list(df, "responses")
+
+        res_df = pd.DataFrame().assign(tag = df["tag"], patterns = df['patterns'], responses = df['responses'], context_set = "")
+
+        json_df = res_df.to_json(orient = "table", index = False)
+
+        parsed_df = json.loads(json_df)
+
+        json_obj = json.dumps(parsed_df, indent = 4)
+
+        with open("intents.json", 'w') as f:
+            f.write(json_obj)
+
+        with open("intents.json", 'r') as f:
+            obj = json.load(f)
+
+        intents = obj['data']
+
+        res = {"intents" : intents}
+
+        with open(new_filename, 'w') as f:
+            f.write(json.dumps(res, indent=4))
+
+    def __get_intents(self, url:str, new_filename:str = "intents.json") -> None:
+        """
+            Will generate new intents file from specified spreadsheet url
+
+            Args:
+                url (str): url containing the spreadsheet to load
+                new_filename (srt): name of intents file, Default = intents.json
+        """
+
+        df = pd.read_csv(url)
+
+        self.__load_spreadsheet_as_intents(df, new_filename=new_filename)
+
+    def __train_agent(self):
+        #implement method to check for last modified date here
+        
+        self.__get_intents(self.SPREADSHEET)
+
+        self.agent = GenericAssistant(self.PATH_TO_INTENTS, model_name='faqAiAgent')
+        
+        if os.path.exists(f'../trained_model/{self.agent.model_name}.h5'):
+            self.agent.load_model(model_name=f'../trained_model/{self.agent.model_name}')
+        else:
+            self.agent.train_model()
+            self.agent.save_model(model_name=f'../trained_model/{self.agent.model_name}')
 
 async def setup(bot):
     await bot.add_cog(Ask(bot))
